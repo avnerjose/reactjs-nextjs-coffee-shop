b00388a85b8731763b6c1df758f970f4eff25579
avnerjose <avnerj.g.r@gmail.com>
2022-09-22 15:35:55 -0300

    Creating build and email jobs in CI

---
+++ .github/scripts/send_email.sh
@@ -0,0 +1,4 @@
+echo "Installing email dependencies"
+sudo apt install mailutils
+echo "Finishing instalation"
+echo "Sending email" | mail -s "CI do Coffee Shop" $EMAIL <<< 'Todas as etapas de CI passaram com sucesso!'
