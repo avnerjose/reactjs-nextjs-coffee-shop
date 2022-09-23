3b9dc252e747985fac01f825fe28a0d912799787
avnerjose <avnerj.g.r@gmail.com>
2022-09-23 18:10:37 -0300

    Creating footer stories

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,10 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Footer } from ".";
+
+export default {
+  component: Footer,
+} as ComponentMeta<typeof Footer>;
+
+const Template: ComponentStory<typeof Footer> = () => <Footer />;
+
+export const Default = Template.bind({});
